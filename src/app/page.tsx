0f7563743ff45d51ff8286bccd5ba4ea7c091769
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-20 12:32:28 +0000

    this will comes after how its works (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <HeroSection />
       <Features />
       <ToolsShowcase />
-      <ToolsByCategory />
       <HowItWorks />
+      <ToolsByCategory />
     </>
   );
 }
